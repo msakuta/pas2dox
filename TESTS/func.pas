a1e13ec10f9f2e4b6d412696fc7a46818ce182ff
sakuta <sakuta>
2011-10-18 20:24:27 +0900

    fixes #71 関数ポインタと普通の関数宣言が混ざっていても解釈できるように修正
    --HG--
    branch : funcptr

--- TESTS/func.pas
+++ TESTS/func.pas
@@ -1,25 +1,52 @@
 {* \file
- * \brief funcions and function pointers
+ * \brief Sample source for funcions and function pointers.
  }
-unit classes;
+unit func;
 
 interface
 
+/// <summary>A normal function declaration.</summary>
+/// <param name="Arg">A typical argument.</param>
+/// <returns>A typical return value.</returns>
 function NormalFunc(Arg: Integer): Integer;
 
+/// <summary>A normal procedure declaration.</summary>
+/// <param name="Arg">A typical argument.</param>
+procedure NormalProc(Arg: Integer);
+
+/// <summary>A type definition for a function pointer.</summary>
+/// <param name="Arg">A typical argument.</param>
+/// <returns>A typical return value.</returns>
 type FuncPtrType = function(Arg: Integer): Integer;
 
 var
+/// <summary>An ordinary variable.</summary>
 avalue: Integer;
 
+/// <summary>A function pointer variable.</summary>
 var FuncPtr: FuncPtrType;
 
+/// <summary>A function pointer variable without typedef.</summary>
+/// <param name="Arg">An in-place function pointer's argument.</param>
+/// <returns>An in-place function pointer's return value.</returns>
 var FuncPtrInPlace: function(Arg: Integer): Integer;
 
+/// <summary>A normal function declaration.</summary>
+/// <param name="Arg">A typical argument.</param>
+/// <returns>A typical return value.</returns>
+function AnotherNormalFunc(Arg: Integer): Integer;
+
+/// <summary>A procedure pointer variable without typedef.</summary>
+/// <param name="Arg">An in-place procedure pointer's argument.</param>
+/// <returns>An in-place procedure pointer's return value.</returns>
 var ProcPtrInPlace:procedure(Arg: Integer);
 
+/// <summary>A function pointer variable without arguments and typedef.</summary>
+/// <returns>An in-place function pointer's return value.</returns>
 var FuncPtrInPlaceWithoutParam:function:Integer;
 
+/// <summary>A procedure pointer variable without arguments and typedef.</summary>
+/// <returns>An in-place procedure pointer's return value.</returns>
 var ProcPtrInPlaceWithoutParam:procedure;
 
 implementation
