e2ee709b261bccc6fd815d5136cd559468c64553
sakuta <sakuta>
2012-11-12 11:05:49 +0900

    Uses 節を #include に変換するようにした
    --HG--
    branch : funcptr
    rename : TESTS/enum.pas => TESTS/using.pas

---
+++ TESTS/using.pas
@@ -0,0 +1,17 @@
+{* \file
+ * \brief An unit using other units
+ }
+unit using;
+
+uses Types, enum;
+
+type
+
+{* A set type of enumeration defined in enum.pas }
+TMsgScanSet = set of TMsgScan;
+
+interface
+
+implementation
+
+end.
